2d27883d6b5e5074017683b4ee7413d1eece3ce4
Chris Dickinson <christopher.s.dickinson@gmail.com>
2010-09-18 15:34:27 -0500

    initial checkin of plate

---
+++ tests/tests.js
@@ -0,0 +1,8 @@
+var includeFile = function(file) {
+    var obj = require('./'+file);
+    for(var i in obj) if(obj.hasOwnProperty(i)) {
+        exports[file + "/" + i] = obj[i];
+    }
+};
+
+includeFile('plate');
