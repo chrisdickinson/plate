2248b944e4c78898b69bfdc32bed67bb8fc8ed27
Chris Dickinson <christopher.s.dickinson@gmail.com>
2012-01-16 17:37:18 -0600

    add a shim for express to allow usage of 'res.render'

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 module.exports = require('./lib/index');
+
+
+try {
+  require('express')
+
+  module.exports.compile = require('./lib/express-shim')
+} catch(err) {
+  console.error(err)
+
+  // do nothing
+}
