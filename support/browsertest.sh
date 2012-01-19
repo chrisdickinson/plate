89b9106795ae8c843161777d40485bb18906b05f
Chris Dickinson <christopher.s.dickinson@gmail.com>
2012-01-19 01:18:22 -0600

    helps to actually make the command work.

--- support/browsertest.sh
+++ support/browsertest.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-curl https://raw.github.com/chrisdickinson/dst.js/master/index.js 2>/dev/null >> tz.js
-python support/build.py plate.js
-node node_modules/.bin/platoon -s -b test.html -I tests/plate.js -I ../dstjs/index.js tests
-
+curl https://raw.github.com/chrisdickinson/dst.js/master/index.js 2>/dev/null > .dst.js
+python support/build.py .plate.js
+node node_modules/.bin/platoon -s -b test.html -I .plate.js -I .dst.js tests
+rm .plate.js
+rm .dst.js
