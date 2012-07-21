3b26b884bc1300b028bac92a16798ebb7f0179f0
Chris Dickinson <christopher.s.dickinson@gmail.com>
2012-07-20 23:03:03 -0700

    browser.js is required for the browser build step

---
+++ browser.js
@@ -0,0 +1,11 @@
+require('dst')
+
+var plate = require('./lib/index')
+if(typeof define !== 'undefined' && define.amd) {
+  define('plate', [], function() { return plate })
+} else {
+  window.plate = plate
+}
+
+plate.utils = plate.date = require('./lib/date')
+plate.libraries = require('./lib/libraries')
